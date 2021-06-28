9545277013b1b34192f684fbe25891e8979f52a0
Ridham <email@example.com>
2021-06-28 21:36:01 +0530

    github pages

---
+++ build/precache-manifest.dc5b0ff51e4899262e8b6a48615066fa.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7bddab5f7be96d03b7123f054c63a11",
+    "url": "/index.html"
+  },
+  {
+    "revision": "34a81563ce161779daf9",
+    "url": "/static/js/2.93295ef4.chunk.js"
+  },
+  {
+    "revision": "0162031acf74d15efbaa",
+    "url": "/static/js/main.c931614e.chunk.js"
+  },
+  {
+    "revision": "caa683f82a40415a9bc1",
+    "url": "/static/js/runtime-main.6a7e7f93.js"
+  }
+]);
